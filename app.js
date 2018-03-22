2fdb20a7f04d4fb011fe26256b9e8e6c34773793
ronthecookie <ronthecookie0101@gmail.com>
2018-03-22 15:46:14 +0200

    helmet

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const port = process.env.port || 3000;
 const path = require("path");
 app.listen(port, () => console.log(`Listening on port ${port}.`));
+app.use(require("helmet")());
 app.use(express.static("static"));
 app.get("/materalize.js", (req, res ) => {
     res.sendFile(path.resolve("node_modules/materialize-css/dist/js/materialize.min.js"));
