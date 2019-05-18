043c449ddb1ad3d47539a04d1db0e3f1bf683483
RONTheCookie <me@ronthecookie.me>
2019-05-18 16:09:30 +0300

    abandoned wz crap

---
+++ static/warzonejson/script.js
@@ -0,0 +1,16 @@
+$(() => {
+    const mapTypes = `Blitz
+CTW
+DTM
+DTW
+FFA
+Infection
+KOTH
+TDM
+Tournament`.split("\n");
+    $("#type-select").on("input", e => {
+        console.log(e);
+        const selected = 
+    });
+
+});
